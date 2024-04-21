b490c213682a10374e4b81118eeaafccc5e6194d
Yimi81 <1548222878@qq.com>
2024-04-21 14:56:54 +0000

    update

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -1,3 +1,3 @@
 export NCCL_P2P_DISABLE="1"
 export NCCL_IB_DISABLE="1"
-deepspeed --include=localhost:2,3,4,5,6,7 pretrain.py --train_args_file hparams/train_args.json
+deepspeed --include=localhost:2,3,4,5,6,7 train.py --train_args_file hparams/train_args.json
